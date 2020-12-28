c3e76c6bdd1b2eb03fdc7d1b09c17c259d73a664
Timo Sutterer <hi@timo-sutterer.de>
2020-12-28 18:39:41 +0100

    Bump version: 0.1.3 → 0.1.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Timo Sutterer"
 author = "Timo Sutterer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.3"
+release = "0.1.4"
 
 
 # -- General configuration ---------------------------------------------------
--- openapigateway/__init__.py
+++ openapigateway/__init__.py
@@ -6,6 +6,6 @@ AWS CDK Construct that creates an Amazon API Gateway HttpApi based on a
 parameterized OpenAPI 3 Document.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from openapigateway.constructs import OpenApiGateway
