d3a7e4f89860ad255a99c12f62a754607bec8f99
Timo Sutterer <hi@timo-sutterer.de>
2020-12-28 16:52:08 +0100

    Bump version to 0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Timo Sutterer"
 author = "Timo Sutterer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.5"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="openapigateway",
-    version="0.0.5",
+    version="0.1.0",
     description=(
         "AWS CDK Construct that creates an Amazon API Gateway HttpApi "
         "based on a parameterized OpenAPI 3 Document."
