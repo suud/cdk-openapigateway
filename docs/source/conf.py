35ff363b429a8e0fbd26149b049b355fd65a3f5c
Timo Sutterer <hi@timo-sutterer.de>
2020-12-29 12:52:23 +0100

    Bump version: 0.2.0 → 0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Timo Sutterer"
 author = "Timo Sutterer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
